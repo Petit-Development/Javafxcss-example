99214248cf42267b1b00e73fdec6bf42bd080d19
Joshua Petitma <joshua@petit.dev>
2021-09-17 08:26:43 -0400

    Create basic css template

--- src/main/java/jfxcss/example/App.java
+++ src/main/java/jfxcss/example/App.java
@@ -1,9 +1,9 @@
 package jfxcss.example;
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -11,13 +11,20 @@ public class App extends Application {
 
   public void start(Stage stage) {
     Pane rootPane = new Pane();
+    addStyleSheet(rootPane);
+
     Scene scene = new Scene(rootPane, 400, 400);
     Rectangle rec = new Rectangle(200, 300);
 
-    rec.setFill(Paint.valueOf("black"));
-
     rootPane.getChildren().add(rec);
     stage.setScene(scene);
+
     stage.show();
+  }
+
+  public void addStyleSheet(Pane node) {
+    URL stylesheetURL = App.class.getResource("main.css");
+    String stylesheet = stylesheetURL.toExternalForm();
+    node.getStylesheets().add(stylesheet);
   }
 }
