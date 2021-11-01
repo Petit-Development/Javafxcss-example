78a9e9cceb2f0308c7d5cee09a47284861255beb
Joshua Petitma <joshua@petit.dev>
2021-11-01 09:56:42 -0400

    Add all examples for basic selection

--- src/main/java/jfxcss/example/App.java
+++ src/main/java/jfxcss/example/App.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import fr.brouillard.oss.cssfx.CSSFX;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -13,16 +14,19 @@ public class App extends Application {
 
   public void start(Stage stage) {
     CSSFX.start();
-    Pane rootPane = new Pane();
+    Pane rootPane = new FlowPane();
     addStyleSheet(rootPane);
 
-    Scene scene = new Scene(rootPane, 500, 500);
-    Rectangle rec = new Rectangle(200, 300);
-    Rectangle rec2 = new Rectangle(100, 50);
+    Scene scene = new Scene(rootPane, 500, 200);
+    Rectangle rec1 = new Rectangle(100, 100);
+    Rectangle rec2 = new Rectangle(100, 100);
+    Rectangle rec3 = new Rectangle(100, 100);
+    Rectangle rec4 = new Rectangle(100, 100);
 
-    rec.getStyleClass().add("primary");
-    rec2.getStyleClass().add("secondary");
-    rootPane.getChildren().addAll(rec, rec2);
+    rec2.getStyleClass().add("primary");
+    rec3.getStyleClass().add("secondary");
+    rec4.setId("title");
+    rootPane.getChildren().addAll(rec1, rec2, rec3, rec4);
     stage.setScene(scene);
 
     stage.show();
