e4d22e8e5fb9384f5b4f005bb06c129e8ab9c777
Joshua Petitma <joshua@petit.dev>
2021-10-31 20:46:36 -0400

    Add example code for selectors

--- src/main/java/jfxcss/example/App.java
+++ src/main/java/jfxcss/example/App.java
@@ -12,14 +12,17 @@ import javafx.stage.Stage;
 public class App extends Application {
 
   public void start(Stage stage) {
-    var s = CSSFX.start();
+    CSSFX.start();
     Pane rootPane = new Pane();
     addStyleSheet(rootPane);
 
-    Scene scene = new Scene(rootPane, 400, 400);
+    Scene scene = new Scene(rootPane, 500, 500);
     Rectangle rec = new Rectangle(200, 300);
+    Rectangle rec2 = new Rectangle(100, 50);
 
-    rootPane.getChildren().add(rec);
+    rec.getStyleClass().add("primary");
+    rec2.getStyleClass().add("secondary");
+    rootPane.getChildren().addAll(rec, rec2);
     stage.setScene(scene);
 
     stage.show();
